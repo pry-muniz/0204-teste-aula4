f40ec774d3f29a68b8da9070962768dfd1294629
pry-muniz <pryy.muniiz@gmail.com>
2020-02-20 15:06:50 -0300

    adicionar ex1.py

---
+++ ex1.py
@@ -0,0 +1,13 @@
+import datetime
+dia_de_nasc = datetime.date(year=1993, month=2, day=20)
+def calcular_dias_de_vida(data):
+    if data is None:
+        erro = 'ops'
+        return error
+    hoje = datetime.date.today()    
+    dias_de_vida = hoje - data
+    return dias_de_vida
+
+retorno = calcular_dias_de_vida(dia_de_nasc)
+print(retorno)
+
